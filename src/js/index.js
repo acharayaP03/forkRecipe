69a36a2a6edf29e4d6341cf0abb3684ce6a48546
acharyaP03 <acharyap03@gmail.com>
2019-11-09 23:01:58 +1100

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// Global app controller
+
+
+import {num, func} from './test';
+function multiply(a,b){
+    return a * b;
+}
+console.log(`Imported number is ${num} and its multilied by 2 is ${multiply(num, 2)}`)
+
+func();
