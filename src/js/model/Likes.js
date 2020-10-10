b73df05131c23ac6b86bd87b207739b755f75f5a
Prabhakar Acharya <acharyaP03@gmail.com>
2020-10-10 12:38:34 +1100

    localstorage

--- src/js/model/Likes.js
+++ src/js/model/Likes.js
@@ -11,12 +11,14 @@
     addLike(id, title, author, img) {
         const like = { id, title, author, img };
         this.likes.push(like);
+        this.persistData();
         return like;
     }
 
     deleteLike(id){
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
+        this.persistData();
     }
 
     isLiked(id){
@@ -25,5 +27,19 @@
 
     getNumLikes(){
         return this.likes.length;
+    }
+
+    //Save recipe to the localStorage.
+    persistData(){
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage(){
+        const storage = JSON.parse(localStorage.getItem('likes'));
+        /**
+         * @localStorage will return null if there is no likes stored 
+         * @solution check if likes are stored before call it 
+         */
+        if(storage) this.likes = storage;
     }
  }
